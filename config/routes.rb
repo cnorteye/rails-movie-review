5d4cbcbd907f00d518f72476f5efc946a98881f6
cnorteye <cnortye@gmail.com>
2020-10-06 16:56:52 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :reviews
+  resources :movies do
+   	resources :reviews, except: [:show, :index]
+   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "movies#index"
+
+end
